5c53c9a8ed8c8dacb7cb605dc8648104775b63fa
Sydxyz <109050076+Sydxyz@users.noreply.github.com>
2023-05-09 09:19:02 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -31,7 +31,7 @@ async def start(client, message):
                     InlineKeyboardButton('⍟ Aʙᴏᴜᴛ', callback_data='about'),
                     InlineKeyboardButton('Iɴʟɪɴᴇ Sᴇᴀʀᴄʜ ☌', switch_inline_query_current_chat='')
                 ],[
-                    InlineKeyboardButton('Mᴀɪɴ Cʜᴀɴɴᴇʟ', url='@Mod_MoviezX),
+                    InlineKeyboardButton('Mᴀɪɴ Cʜᴀɴɴᴇʟ', url='@Mod_MoviezX'),
                     InlineKeyboardButton('✇ Jᴏɪɴ Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ ✇', url=CHNL_LNK)
                   ]]
         reply_markup = InlineKeyboardMarkup(buttons)
@@ -61,7 +61,7 @@ async def start(client, message):
                     InlineKeyboardButton('⍟ Aʙᴏᴜᴛ', callback_data='about'),
                     InlineKeyboardButton('Iɴʟɪɴᴇ Sᴇᴀʀᴄʜ ☌', switch_inline_query_current_chat='')
                 ],[
-                    InlineKeyboardButton('&', url='@bot_cracker),
+                    InlineKeyboardButton('&', url='@bot_cracker'),
                     InlineKeyboardButton('✇ Jᴏɪɴ Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ ✇', url=CHNL_LNK)
                   ]]
         reply_markup = InlineKeyboardMarkup(buttons)
