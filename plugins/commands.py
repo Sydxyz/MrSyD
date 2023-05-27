239355317ad580c31a0425a1aff14e259ba53365
Sydxyz <109050076+Sydxyz@users.noreply.github.com>
2023-05-27 20:20:47 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -79,7 +79,7 @@ async def start(client, message):
         btn = [
             [
                 InlineKeyboardButton(
-                    "❆ Jᴏɪɴ Oᴜʀ Bᴀᴄᴋ-Uᴘ Cʜᴀɴɴᴇʟ ❆", url=ChatJoinRequest.invite_link
+                    "❆ Jᴏɪɴ Oᴜʀ Bᴀᴄᴋ-Uᴘ Cʜᴀɴɴᴇʟ ❆", url=ChatInviteLink.invite_link
                 )
             ]
         ]
