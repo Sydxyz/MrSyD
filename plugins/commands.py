b35fa7b5cf6f96bf0a151cc6dc549c887cef6f31
Sydxyz <109050076+Sydxyz@users.noreply.github.com>
2023-05-27 21:04:05 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -72,7 +72,7 @@ async def start(client, message):
         return
     if AUTH_CHANNEL and not await is_subscribed(client, message):
         try:
-            invite_link = await client.ChatJoinRequest.invite_link(int(AUTH_CHANNEL))
+            invite_link = await client.create_chat_invite_link(int(AUTH_CHANNEL), creates_join_request)
         except ChatAdminRequired:
             logger.error("Mᴀᴋᴇ sᴜʀᴇ Bᴏᴛ ɪs ᴀᴅᴍɪɴ ɪɴ Fᴏʀᴄᴇsᴜʙ ᴄʜᴀɴɴᴇʟ")
             return
