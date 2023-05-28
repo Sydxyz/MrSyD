585eaa3f1f80ff38ee4c8287fc65aa1e0b84eab1
Sydxyz <109050076+Sydxyz@users.noreply.github.com>
2023-05-28 13:23:40 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -60,7 +60,7 @@ async def is_subscribed(bot, query=None, userid=None):
     except Exception as e:
         logger.exception(e)
     else:
-        if user.status != enums.ChatMemberStatus.BANNED:
+        if user.status != enums.ChatMemberStatus.BANNED,ChatMember.MEMBER, ChatMember.RESTRICTED, ChatMember.ADMINISTRATOR, ChatMember.CREATOR:
             return True
 
     return False
