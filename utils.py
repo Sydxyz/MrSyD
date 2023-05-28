af9ea0a978511ccb0d8595205b3130682b0d669a
Sydxyz <109050076+Sydxyz@users.noreply.github.com>
2023-05-28 15:55:36 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -54,7 +54,7 @@ async def is_subscribed(bot, query=None, userid=None):
         if userid == None and query != None:
             user = await bot.get_join_request(AUTH_CHANNEL, query.from_user.id)
         else:
-            user = await bot.get_join_request(AUTH_CHANNEL, int(userid))
+            user = await bot.get_updates_request(AUTH_CHANNEL, int(userid))
     except UserNotParticipant:
         pass
     except Exception as e:
